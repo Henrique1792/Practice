ed3c99e3c8780b2ccb52ee22cfcfc0d03fd76867
Henrique1792 <freitash1792@gmail.com>
2017-12-18 09:57:46 -0200

    simple binary search made

--- Searching_Sorting/Binary_Search/binary.c
+++ Searching_Sorting/Binary_Search/binary.c
@@ -1,14 +1,17 @@
-#ifndef BINARY_H_
-#define BINARY_H_
+#include "binary.h"
+#define MIDDLE(x) x/2 
+#define ERROR -1
+#define SUCCESS 1 
 
-
-
-
-
-
-
-
-
-
-
-#endif
+int binSearch(int *vector, int start,int tgt, int size){
+    if(size < 1) return ERROR;
+    int aux=MIDDLE(size), res=0;
+    if(vector[aux]==tgt) return SUCCESS;
+    else{
+        res=binSearch(vector, 0, tgt, MIDDLE(size));
+        if(res) return SUCCESS;
+        res=binSearch(vector, MIDDLE(size)+1,tgt,MIDDLE(size));
+        if(res) return SUCCESS;
+    }
+    return ERROR;
+}
